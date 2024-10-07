ee17246da86ea5c18569f7d2695c89f043c40f7a
ivnzdrs <ivandres.student@ua.edu.ph>
2024-10-07 23:15:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+function Key({ label, onClick, id }) {
+  return (
+    <button onClick={() => onClick(label)} id={id}>
+      {label}
+    </button>
+  );
+}
+
+function Display({ value }) {
+  return (
+    <div className="Display">
+      {value}
+    </div>
+  );
+}
+
 function App() {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [calculatorInput, setCalculatorInput] = useState('');
+
+  const handleButtonClick = (label) => {
+    if (label === 'C') {
+      setDisplayValue('0');
+      setCalculatorInput('');
+    } else if (label === '=') {
+      try {
+        setDisplayValue(eval(calculatorInput));
+      } catch {
+        setDisplayValue('Syntax Error');
+      }
+    } else {
+      const newInput = calculatorInput + label;
+      setCalculatorInput(newInput);
+      setDisplayValue(newInput);
+    }
+  };
+
+  const handleSurnameClick = () => {
+    setDisplayValue('Ivan Andres');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator of Ivan Andres - IT3A</h1> {}
+      <div className="CalcContainer">
+        <div className="DispContainer">
+          <Display value={displayValue} />
+        </div>
+        <div className="ButtonsContainer">
+          <div className="btn-box1">
+            <Key label={7} onClick={handleButtonClick} />
+            <Key label={8} onClick={handleButtonClick} />
+            <Key label={9} onClick={handleButtonClick} />
+            <Key label={'/'} onClick={handleButtonClick} />
+          </div>
+          <div className="btn-box2">
+            <Key label={4} onClick={handleButtonClick} />
+            <Key label={5} onClick={handleButtonClick} />
+            <Key label={6} onClick={handleButtonClick} />
+            <Key label={'*'} onClick={handleButtonClick} />
+          </div>
+          <div className="btn-box3">
+            <Key label={1} onClick={handleButtonClick} />
+            <Key label={2} onClick={handleButtonClick} />
+            <Key label={3} onClick={handleButtonClick} />
+            <Key label={'-'} onClick={handleButtonClick} />
+          </div>
+          <div className="btn-box4">
+            <Key label={'C'} onClick={handleButtonClick}/>
+            <Key label={0} onClick={handleButtonClick} />
+            <Key label={'='} onClick={handleButtonClick} />
+            <Key label={'+'} onClick={handleButtonClick} />
+          </div>
+        </div>
+        <div className="btn-box5">
+          <Key label={'ANDRES'} onClick={handleSurnameClick} id="surname" />
+        </div>
+      </div>
     </div>
   );
 }
